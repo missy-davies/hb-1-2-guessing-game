8fca515d3d38858cde1baec1fbf059088c86b320
Missy Davies <ms.melissadavies@gmail.com>
2021-02-24 00:24:02 +0000

    Added where user

--- game.py
+++ game.py
@@ -1,11 +1,12 @@
 # number guessing game
 
 import random
-num = random.randint(1,100)
 
-def play_game(num):
+def play_game():
+    num = random.randint(1,100)
     print("Howdy, what's your name? ")
     name = input("(Type in your name)")
+    name = name.title()
     print(f"{name} I'm thinking of a number between 1 and 100.")
     print("Try to guess my number.")
     num_tries = 0
@@ -16,20 +17,22 @@ def play_game(num):
         if guess == num:
             print(f"Well done, {name}! You found my number in {num_tries} tries!")
             break 
-        elif guess > num:
+        elif guess > num and guess <= 100:
             print("Your guess is too high, try again.")
-        elif guess < num:
+        elif guess < num and guess > 0:
             print("Your guess is too low, try again.")
+        elif guess < 1 or guess > 100:
+            print("Your guess is out of range! Please make sure you pick a number between 1 and 100.")
     while True:
         print("Wanna play again?")
         answer = input("> ")
         answer = answer.lower()
         if answer.startswith("y"):
-            play_game(num)
+            play_game()
         else:
             print("Okay, bye!")
             break
-play_game(num)
+play_game()
 
 
 
